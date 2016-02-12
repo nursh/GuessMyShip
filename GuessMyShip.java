800447a5f6ba9e4f686f18ef058767140d2261a9
nursh <nursh@github.com>
2016-02-12 14:36:06 -0700

    added main class

---
+++ GuessMyShip.java
@@ -0,0 +1,13 @@
+public class GuessMyShip{
+
+	public static final String HIT = "hit";
+	public static final String MISS = "miss";
+	public static final String KILL = "kill";
+	private int hits;
+	private int[] shipLocation;
+
+	public static void main(String[] args) {
+		
+	}
+
+}
